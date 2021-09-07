15c418530264c52c72684e2488121d29b197b959
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-07 21:10:43 +0530

    ADDED: method to validate first mandatory part of email

--- src/com/bridgelabz/emailvalidation/EmailValidation.java
+++ src/com/bridgelabz/emailvalidation/EmailValidation.java
@@ -1,11 +1,28 @@
 package com.bridgelabz.emailvalidation;
 
 import java.util.Scanner;
+import java.util.regex.*;
 
 public class EmailValidation {
 
 	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter the email");
+		String email = scanner.nextLine();
+		System.out.println("Is the email valid?");
+		if(checkForValidEmail(email))
+			System.out.println("It is valid");
+		else
+			System.out.println("It is invalid");
+		scanner.close();
 
+	}
+
+	private static boolean checkForValidEmail(String email) {
+		String regex = "[a-zA-Z][a-zA-Z0-9_-]*[a-zA-Z0-9]";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(email);
+		return matcher.matches();
 	}
 
 }
